db56991387fa6a9da4e90eb765124f22acf0727c
Ryan Knight <ryan@grandcloud.com>
2025-08-26 16:03:48 -0600

    duckdb

--- data_pipeline/writers/archive_elasticsearch/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Elasticsearch writer package.
-
-This package contains the Elasticsearch orchestrator for entity-specific indexing.
-"""
-
-from .elasticsearch_orchestrator import ElasticsearchOrchestrator
-
-__all__ = ["ElasticsearchOrchestrator"]
