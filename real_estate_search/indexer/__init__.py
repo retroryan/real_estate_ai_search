8a7c9324f06ba13fbbb9fd18cf20f75424f4c877
Ryan Knight <ryan@grandcloud.com>
2025-08-28 17:11:40 -0600

    clean up

--- real_estate_search/indexer/__init__.py
+++ real_estate_search/indexer/__init__.py
@@ -1,6 +1,3 @@
 """Property indexer module for Elasticsearch."""
 
-# PropertyIndexer is deprecated - use IndexingService with DependencyContainer instead
-# from .property_indexer import PropertyIndexer
-
 __all__ = []
