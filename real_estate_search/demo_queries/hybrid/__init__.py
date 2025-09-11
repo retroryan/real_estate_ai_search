a8c56d95e3ba35615f8a7b9a182a89536b1b5dde
Ryan Knight <ryan@grandcloud.com>
2025-09-10 22:35:46 -0600

    clean up

--- real_estate_search/demo_queries/hybrid/__init__.py
+++ real_estate_search/demo_queries/hybrid/__init__.py
@@ -3,6 +3,5 @@ Hybrid search query builders and utilities.
 """
 
 from .query_builder import HybridQueryBuilder
-from .demo_runner import HybridDemoRunner
 
-__all__ = ['HybridQueryBuilder', 'HybridDemoRunner']
+__all__ = ['HybridQueryBuilder']
